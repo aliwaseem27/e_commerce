a60ce5ac03cadef2314bc5f0c87183c38750a288
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-05 02:39:38 +0300

    add: HomeCategories

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -2,13 +2,21 @@ import 'package:e_commerce/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:e_commerce/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/text_strings.dart';
+import 'package:e_commerce/utils/device/device_utility.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/curved_edge_widget.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
 import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
 import '../../../../utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -25,10 +33,25 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   // AppBar
                   THomeAppBar(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
 
                   // SearchBar
+                  TSearchContainer(text: 'Search in Store'),
+                  const SizedBox(height: TSizes.spaceBtwSections),
 
                   // Categories
+                  Padding(
+                    padding: const EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        const TSectionHeading(
+                            title: "Popular Categories", showActionButton: false, textColor: TColors.white),
+                        const SizedBox(height: TSizes.spaceBtwItems),
+                        HomeCategories(),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             )
