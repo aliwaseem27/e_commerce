c4128934635fd599d459ec68a1eea8f9535d5552
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-12 16:01:17 +0300

    add: SettingsSecreen Body

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -1,8 +1,8 @@
 import 'package:e_commerce/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
-import 'package:e_commerce/common/widgets/images/t_circular_image.dart';
+import 'package:e_commerce/common/widgets/list_tiles/settings_menu_tile.dart';
+import 'package:e_commerce/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
-import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
@@ -31,18 +31,86 @@ class SettingsScreen extends StatelessWidget {
                   ),
 
                   // User Profile card
-                  TUserProfileTile(),
+                  const TUserProfileTile(),
                   const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             ),
 
             // Body
+            Padding(
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  // Account Settings
+                  const TSectionHeading(title: "Account Settings", showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+
+                  const TSettingsMenuTile(
+                      icon: Iconsax.safe_home, title: "My Addresses", subTitle: "Set shopping delivery address"),
+                  const TSettingsMenuTile(
+                      icon: Iconsax.shopping_cart,
+                      title: "My Cart",
+                      subTitle: "Add, remove products and move to checkout"),
+                  const TSettingsMenuTile(
+                      icon: Iconsax.bag_tick, title: "My Orders", subTitle: "In-progress and Completed Orders"),
+                  const TSettingsMenuTile(
+                      icon: Iconsax.bank,
+                      title: "Bank Account",
+                      subTitle: "Withdraw balance to registered bank account"),
+                  const TSettingsMenuTile(
+                      icon: Iconsax.discount_shape,
+                      title: "My Coupons",
+                      subTitle: "List of all the discounted coupons"),
+                  const TSettingsMenuTile(
+                      icon: Iconsax.notification,
+                      title: "Notification",
+                      subTitle: "Set any kind of notification message"),
+                  const TSettingsMenuTile(
+                      icon: Iconsax.security_card,
+                      title: "Account Privacy",
+                      subTitle: "Manage data usage and connected accounts"),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  // App Settings
+                  const TSectionHeading(title: "App Settings", showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  const TSettingsMenuTile(
+                      icon: Iconsax.document_upload,
+                      title: "Load Data",
+                      subTitle: "Upload Data to your Cloud Firebase"),
+                  TSettingsMenuTile(
+                    icon: Iconsax.location,
+                    title: "Geolocation",
+                    subTitle: "Set recommendation based on location",
+                    trailing: Switch(value: true, onChanged: (value) {}),
+                  ),
+                  TSettingsMenuTile(
+                    icon: Iconsax.security_user,
+                    title: "Safe Mode",
+                    subTitle: "Search result is safe for all ages",
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                  ),
+                  TSettingsMenuTile(
+                    icon: Iconsax.image,
+                    title: "HD Image Quality",
+                    subTitle: "Set image quality to be seen",
+                    trailing: Switch(value: false, onChanged: (value) {}),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  // Logout Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(onPressed: () {}, child: const Text("Logout")),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections * 1.5),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
