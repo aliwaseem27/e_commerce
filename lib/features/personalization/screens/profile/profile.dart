526480e450acd8b4663ebac25119adb6f9db01ff
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-15 23:59:27 +0300

    login

--- lib/features/personalization/screens/profile/profile.dart
+++ lib/features/personalization/screens/profile/profile.dart
@@ -6,7 +6,7 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: TAppBar(title: Text("Profile"), showBackArrow: true),
       body: SingleChildScrollView(),
     );
