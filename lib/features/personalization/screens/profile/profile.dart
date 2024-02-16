c042d6e8461147781d84901ff4b92e812b1b2342
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-16 19:31:56 +0300

    add: Profile Edit Screen

--- lib/features/personalization/screens/profile/profile.dart
+++ lib/features/personalization/screens/profile/profile.dart
@@ -1,14 +1,72 @@
 import 'package:e_commerce/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce/common/widgets/images/t_circular_image.dart';
+import 'package:e_commerce/common/widgets/texts/section_heading.dart';
+import 'package:e_commerce/features/personalization/screens/profile/widgets/profile_menu.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TAppBar(title: Text("Profile"), showBackArrow: true),
-      body: SingleChildScrollView(),
+    return Scaffold(
+      appBar: const TAppBar(title: Text("Profile"), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              // Profile Picture
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const TCircularImage(image: TImages.user, width: 80, height: 80),
+                    TextButton(onPressed: () {}, child: const Text("Change Profile Picture")),
+                  ],
+                ),
+              ),
+
+              // Details
+              const SizedBox(height: TSizes.spaceBtwItems / 2),
+              const Divider(),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              const TSectionHeading(title: "Profile Information", showActionButton: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              TProfileMenu(title: "Name", value: "Ali Waseem", onPressed: () {}),
+              TProfileMenu(title: "Username", value: "aliwaseem27", onPressed: () {}),
+
+              const SizedBox(height: TSizes.spaceBtwItems),
+              const Divider(),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              // Heading Personal Info
+              const TSectionHeading(title: "Personal Information", showActionButton: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              TProfileMenu(title: "User ID", value: "45689", icon: Iconsax.copy, onPressed: () {}),
+              TProfileMenu(title: "E-mail", value: "ali.waseem27@gmail.com", onPressed: () {}),
+              TProfileMenu(title: "Phone Number", value: "+9647845460377", onPressed: () {}),
+              TProfileMenu(title: "Gender", value: "Male", onPressed: () {}),
+              TProfileMenu(title: "Date of Birth", value: "2 Jul, 1995", onPressed: () {}),
+
+              const Divider(),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Close Account", style: TextStyle(color: Colors.red)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
