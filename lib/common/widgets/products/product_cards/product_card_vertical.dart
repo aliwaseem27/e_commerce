256e091a5ed685f5c60863d79d893798b8f211a6
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-16 22:56:51 +0300

    add: Product Details Image Slider

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -2,11 +2,13 @@
 import 'package:e_commerce/common/styles/shadows.dart';
 import 'package:e_commerce/common/widgets/images/t_rounded_image.dart';
 import 'package:e_commerce/common/widgets/texts/product_title_text.dart';
+import 'package:e_commerce/features/shop/screens/product_details/product_detail.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../custom_shapes/containers/t_rounded_container.dart';
@@ -22,7 +24,7 @@ class TProductCardVertical extends StatelessWidget {
     final dark = THelperFunctions.isDarkMode(context);
 
     return GestureDetector(
-      onTap: () {},
+      onTap: () => Get.to(()=> const ProductDetail()),
       child: Container(
         width: 180,
         padding: const EdgeInsets.all(1),
---
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -0,0 +1,31 @@
+import 'package:e_commerce/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce/common/widgets/custom_shapes/curved_edges/curved_edge_widget.dart';
+import 'package:e_commerce/common/widgets/icons/t_circular_icon.dart';
+import 'package:e_commerce/common/widgets/images/t_rounded_image.dart';
+import 'package:e_commerce/features/shop/screens/product_details/widgets/image_slider.dart';
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Product Image Slider
+            TProductImageSlider(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
