a8cf0358d7148fff1539a24c9241730e57e14030
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-17 22:41:14 +0300

    finish: Product Details Screen

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,9 +1,13 @@
+import 'package:e_commerce/common/widgets/texts/section_heading.dart';
+import 'package:e_commerce/features/shop/screens/product_details/widgets/bottom_add_to_cart.dart';
 import 'package:e_commerce/features/shop/screens/product_details/widgets/image_slider.dart';
+import 'package:e_commerce/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:e_commerce/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:e_commerce/features/shop/screens/product_details/widgets/rating_share_widget.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetail extends StatelessWidget {
   const ProductDetail({super.key});
@@ -11,7 +15,8 @@ class ProductDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -34,9 +39,38 @@ class ProductDetail extends StatelessWidget {
                   TProductMetaData(),
 
                   // Attributes
+                  TProductAttributes(),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
                   // Checkout Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Checkout"),
+                    ),
+                  ),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
                   // Description
+                  TSectionHeading(title: "Description", showActionButton: false),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  ReadMoreText(
+                    "This is a Product description for Blue Nike Sleeve less vest. There are more things that can be added but I am just practicing and nothing else.",
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: " Show more",
+                    trimExpandedText: " Less",
+                    moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
                   // Reviews
+                  Divider(),
+                  SizedBox(height: TSizes.spaceBtwSections),
+                  TSectionHeading(title: "Reviews(199)", onPressed: () {}),
+                  SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             )
@@ -46,4 +80,3 @@ class ProductDetail extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/features/shop/screens/product_details/widgets/product_attributes.dart
@@ -0,0 +1,117 @@
+import 'package:e_commerce/common/widgets/chips/choice_chip.dart';
+import 'package:e_commerce/common/widgets/custom_shapes/containers/t_rounded_container.dart';
+import 'package:e_commerce/common/widgets/texts/product_price_text.dart';
+import 'package:e_commerce/common/widgets/texts/product_title_text.dart';
+import 'package:e_commerce/common/widgets/texts/section_heading.dart';
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class TProductAttributes extends StatelessWidget {
+  const TProductAttributes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Column(
+      children: [
+        // Selected Attribute Pricing and Description
+        TRoundedContainer(
+          padding: const EdgeInsets.all(TSizes.md),
+          backgroundColor: dark ? TColors.darkerGrey : TColors.grey,
+          child: Column(
+            children: [
+              // Title, Price & Stock Status
+              Row(
+                children: [
+                  TSectionHeading(title: "Variation", showActionButton: false),
+                  SizedBox(width: TSizes.spaceBtwItems),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const TProductTitleText(title: "Price : ", smallSize: true),
+
+                          // Actual Price
+                          Text(
+                            "\$25",
+                            style:
+                                Theme.of(context).textTheme.titleSmall!.apply(decoration: TextDecoration.lineThrough),
+                          ),
+                          const SizedBox(width: TSizes.spaceBtwItems),
+
+                          // Sale Price
+                          const TProductPriceText(price: "20"),
+                        ],
+                      ),
+
+                      // Stock
+                      Row(
+                        children: [
+                          TProductTitleText(title: "Stock : ", smallSize: true),
+                          Text("In Stock", style: Theme.of(context).textTheme.titleMedium),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              // Variation Description
+              const TProductTitleText(
+                title: "This is the Description of the Product and it can go up to max 4 lines.",
+                smallSize: true,
+                maxLines: 4,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+
+        // Attributes
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TSectionHeading(title: "Colors", showActionButton: false),
+            SizedBox(height: TSizes.spaceBtwItems / 2),
+            Wrap(
+              spacing: 8,
+              children: [
+                TChoiceChip(text: "Green", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "Blue", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "Yellow", selected: true, onSelected: (value) {}),
+                TChoiceChip(text: "Green", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "Blue", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "Yellow", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "Green", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "Blue", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "Yellow", selected: false, onSelected: (value) {}),
+              ],
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TSectionHeading(title: "Size", showActionButton: false),
+            SizedBox(height: TSizes.spaceBtwItems / 2),
+            Wrap(
+              spacing: 8,
+              children: [
+                TChoiceChip(text: "EU 32", selected: true, onSelected: (value) {}),
+                TChoiceChip(text: "EU 34", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "EU 36", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "EU 38", selected: true, onSelected: (value) {}),
+                TChoiceChip(text: "EU 40", selected: false, onSelected: (value) {}),
+                TChoiceChip(text: "EU 42", selected: false, onSelected: (value) {}),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
