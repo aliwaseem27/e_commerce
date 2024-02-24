82f6c6a9db3c4d57b8573051a22365e579ca1acd
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-24 23:27:19 +0300

    add: Firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  // TODO: Add Widgets Bindings
+  // TODO: Init Local Storage
+  // TODO: Init Payment Methods
+  // TODO: Await Native Splash
+
+  // TODO: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // TODO: Initialize Authentication
+
+  // Load all Material Design / Themes / Localizations / Bindings
   runApp(const MyApp());
 }
-
