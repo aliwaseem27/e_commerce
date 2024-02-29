a5e529ed0828d24ce6e6ed700045903074b9c25b
aliwaseem27 <ali.waseem27@gmail.com>
2024-02-29 23:55:45 +0300

    fill

---
+++ lib/common/widgets/poppings/fill.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Filling extends StatelessWidget {
+  const Filling({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholde();
+    r();
+  }
+}
