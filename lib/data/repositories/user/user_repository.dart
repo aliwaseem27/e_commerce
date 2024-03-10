ea08b454733a238d7e5b697b3c9a48ec0547762b
aliwaseem27 <ali.waseem27@gmail.com>
2024-03-10 22:21:58 +0300

    update: UserRepository

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce/data/repositories/authentication/authentication_repository.dart';
 import 'package:e_commerce/features/authentication/models/user_model.dart';
 import 'package:e_commerce/utils/exceptions/firebase_exceptions.dart';
 import 'package:e_commerce/utils/exceptions/format_exceptions.dart';
@@ -27,12 +28,69 @@ class UserRepository extends GetxController {
   }
 
   /// Function to fetch user details based on user ID.
+  Future<UserModel> fetchUserDetails() async {
+    try {
+      final documentSnapshot = await _db.collection("Users").doc(AuthenticationRepository.instance.authUser?.uid).get();
+      if(documentSnapshot.exists){
+        return UserModel.fromSnapshot(documentSnapshot);
+      } else {
+        return UserModel.empty();
+      }
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw "Something went wrong. Please try again.";
+    }
+  }
 
   /// Function to update user data in Firestore.
+  Future<void> updateUserDetails(UserModel updatedUser) async {
+    try {
+      await _db.collection("Users").doc(updatedUser.id).update(updatedUser.toJson());
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw "Something went wrong. Please try again.";
+    }
+  }
 
   /// Update any field inn specific Users Collection.
+  Future<void> updateSingleField(Map<String, dynamic> json) async {
+    try {
+      await _db.collection("Users").doc(AuthenticationRepository.instance.authUser?.uid).update(json);
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw "Something went wrong. Please try again.";
+    }
+  }
 
   /// Upload any Image.
 
   /// Function to remove user data from Firestore.
+  Future<void> removeUserRecord(String userId) async {
+    try {
+      await _db.collection("Users").doc(userId).delete();
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw "Something went wrong. Please try again.";
+    }
+  }
 }
--- lib/features/authentication/models/user_model.dart
+++ lib/features/authentication/models/user_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce/utils/formatters/formatter.dart';
 
 /// Model class representing user data
@@ -36,6 +37,7 @@ class UserModel {
     return usernameWithPrefix;
   }
 
+  // Static function to create an empty user model.
   static UserModel empty() => UserModel(
         id: "",
         firstName: "",
@@ -46,6 +48,7 @@ class UserModel {
         profilePicture: "",
       );
 
+  // Convert model to JSON structure for storing data in Firebase
   Map<String, dynamic> toJson() {
     return {
       "FirstName": firstName,
@@ -57,18 +60,21 @@ class UserModel {
     };
   }
 
-  // factory UserModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document) {
-  //   if (document.data() != null) {
-  //     final data = document.data()!;
-  //     return UserModel(
-  //       id: document.id,
-  //       firstName: data["FirstName"] ?? "",
-  //       lastName: data["LastName"] ?? "",
-  //       username: data["Username"] ?? "",
-  //       email: data["Email"] ?? "",
-  //       phoneNumber: data["PhoneNumber"] ?? "",
-  //       profilePicture: data["ProfilePicture"] ?? "",
-  //     );
-  //   }
-  // }
+  // Factory method to create a UserModel from a Firebase document snapshot.
+  factory UserModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final data = document.data()!;
+      return UserModel(
+        id: document.id,
+        firstName: data["FirstName"] ?? "",
+        lastName: data["LastName"] ?? "",
+        username: data["Username"] ?? "",
+        email: data["Email"] ?? "",
+        phoneNumber: data["PhoneNumber"] ?? "",
+        profilePicture: data["ProfilePicture"] ?? "",
+      );
+    } else {
+      return UserModel.empty();
+    }
+  }
 }
