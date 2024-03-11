4e4edec65fa3aeab72b36a4124f78bf78068e16f
aliwaseem27 <ali.waseem27@gmail.com>
2024-03-11 23:41:10 +0300

    add: fetchUserRecord

--- lib/features/personalization/controllers/user_controller.dart
+++ lib/features/personalization/controllers/user_controller.dart
@@ -9,6 +9,29 @@ class UserController extends GetxController {
 
   final userRepository = Get.put(UserRepository());
 
+  Rx<UserModel> user = UserModel.empty().obs;
+
+  final profileLoading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchUserRecord();
+  }
+
+  // Fetch user record
+  Future<void> fetchUserRecord() async {
+    try {
+      profileLoading.value = true;
+      final user = await userRepository.fetchUserDetails();
+      this.user(user);
+    } catch (e) {
+      user(UserModel.empty());
+    } finally {
+      profileLoading.value = false;
+    }
+  }
+
   // Save user record from any Registration provider.
   Future<void> saveUserRecord(UserCredential? userCredential) async {
     try {
--- lib/features/shop/screens/home/widgets/home_appbar.dart
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -1,4 +1,6 @@
+import 'package:e_commerce/features/personalization/controllers/user_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../../common/widgets/appbar/appbar.dart';
 import '../../../../../common/widgets/products/cart/cart_menu_icon.dart';
@@ -12,18 +14,22 @@ class THomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(UserController());
     return TAppBar(
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(TTexts.homeAppbarTitle,
-              style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)),
-          Text(TTexts.homeAppbarSubTitle,
-              style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)),
+          Text(TTexts.homeAppbarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)),
+          Obx(() {
+            return Text(
+              controller.user.value.fullName,
+              style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white),
+            );
+          }),
         ],
       ),
       actions: [
-        TCartCounterIcon(onPressed: (){}, iconColor: TColors.white),
+        TCartCounterIcon(onPressed: () {}, iconColor: TColors.white),
       ],
     );
   }
