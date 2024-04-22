b39a29de0fd5998296cbffb2fc5aa5451ef131d0
aliwaseem27 <ali.waseem27@gmail.com>
2024-04-22 03:56:18 +0300

    add: Category Model

---
+++ lib/data/repositories/categories/category_repository.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class CategoryRepository extends GetxController {
+  static CategoryRepository get instance => Get.find();
+
+  // variables
+  final _db = FirebaseFirestore.instance;
+
+// Get all categories
+
+// Get Sub Categories
+
+// Upload Categories to the Cloud Firebase
+}
